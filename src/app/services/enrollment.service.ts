63d7b1b7061f3710364ccc3fc6f852ea1424188f
Gabriele Giovannangeli <giovannangeligabriele@gmail.com>
2025-11-25 09:50:04 +0100

    fix gitignore

--- src/app/services/enrollment.service.ts
+++ src/app/services/enrollment.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { StudenteDTO } from './studente.service';
-import { CorsoDTO } from './corso.service';
+import { StudenteDTO } from './student.service';
+import { CorsoDTO } from './courses.service';
+
 
 export interface EnrollmentDTO {
   idEnrollment: number;
